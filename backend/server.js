f4177a55d1d04a94335e3f975139b6b79206924c
Abhay Bisht <abhay@webage.com>
2024-07-01 14:26:10 -0400

    server stuff

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,167 @@
 const express = require('express');
+const dotenv = require('dotenv');
 const app = express();
 const PORT = 3000;
 
+dotenv.config();
 app.use(express.json());
 
+const mongoURL = process.env.MONGO_URL;
+const mongoDB = process.env.MONGO_DB;
+const filmCollection = 'films';
+const characterCollection = 'characters';
+const planetCollection = 'planets';
+
 app.get('/api/planets', async (req, res) => {
+    try {
+        const client = await MongoClient.connect(mongoURL);
+        const db = client.db(mongoDB);
+        const data = await db.collection()
+        // const jsonObj = JSON.parse("hello");
+        const jsonObj = {'name':'David'}
+        res.json(jsonObj);
+    } catch (err) {
+        console.error("Error:", err);
+        res.status(500).send("Hmmm, something smells... No socks for you! ☹");
+    }
+});
+
+app.get('/api/characters', async (req, res) => {
+    try {
+        // Console log the entire request object
+        console.log(req);
+
+        // const data = await fs.readFile('../data/socks.json', 'utf8');
+        // const jsonObj = JSON.parse("hello");
+        const jsonObj = {'name':'David'}
+        res.json(jsonObj);
+    } catch (err) {
+        console.error("Error:", err);
+        res.status(500).send("Hmmm, something smells... No socks for you! ☹");
+    }
+});
+
+app.get('/api/films', async (req, res) => {
+    try {
+        // Console log the entire request object
+        console.log(req);
+
+        // const data = await fs.readFile('../data/socks.json', 'utf8');
+        // const jsonObj = JSON.parse("hello");
+        const jsonObj = {'name':'David'}
+        res.json(jsonObj);
+    } catch (err) {
+        console.error("Error:", err);
+        res.status(500).send("Hmmm, something smells... No socks for you! ☹");
+    }
+});
+
+app.get('/api/characters/:id', async (req, res) => {
+    try {
+        // Console log the entire request object
+        console.log(req);
+
+        // const data = await fs.readFile('../data/socks.json', 'utf8');
+        // const jsonObj = JSON.parse("hello");
+        const jsonObj = {'name':'David'}
+        res.json(jsonObj);
+    } catch (err) {
+        console.error("Error:", err);
+        res.status(500).send("Hmmm, something smells... No socks for you! ☹");
+    }
+});
+
+app.get('/api/films/:id', async (req, res) => {
+    try {
+        // Console log the entire request object
+        console.log(req);
+
+        // const data = await fs.readFile('../data/socks.json', 'utf8');
+        // const jsonObj = JSON.parse("hello");
+        const jsonObj = {'name':'David'}
+        res.json(jsonObj);
+    } catch (err) {
+        console.error("Error:", err);
+        res.status(500).send("Hmmm, something smells... No socks for you! ☹");
+    }
+});
+
+app.get('/api/planets/:id', async (req, res) => {
+    try {
+        // Console log the entire request object
+        console.log(req);
+
+        // const data = await fs.readFile('../data/socks.json', 'utf8');
+        // const jsonObj = JSON.parse("hello");
+        const jsonObj = {'name':'David'}
+        res.json(jsonObj);
+    } catch (err) {
+        console.error("Error:", err);
+        res.status(500).send("Hmmm, something smells... No socks for you! ☹");
+    }
+});
+
+app.get('/api/films/:id/characters', async (req, res) => {
+    try {
+        // Console log the entire request object
+        console.log(req);
+
+        // const data = await fs.readFile('../data/socks.json', 'utf8');
+        // const jsonObj = JSON.parse("hello");
+        const jsonObj = {'name':'David'}
+        res.json(jsonObj);
+    } catch (err) {
+        console.error("Error:", err);
+        res.status(500).send("Hmmm, something smells... No socks for you! ☹");
+    }
+});
+
+app.get('/api/films/:id/planets', async (req, res) => {
+    try {
+        // Console log the entire request object
+        console.log(req);
+
+        // const data = await fs.readFile('../data/socks.json', 'utf8');
+        // const jsonObj = JSON.parse("hello");
+        const jsonObj = {'name':'David'}
+        res.json(jsonObj);
+    } catch (err) {
+        console.error("Error:", err);
+        res.status(500).send("Hmmm, something smells... No socks for you! ☹");
+    }
+});
+
+app.get('/api/characters/:id/films', async (req, res) => {
+    try {
+        // Console log the entire request object
+        console.log(req);
+
+        // const data = await fs.readFile('../data/socks.json', 'utf8');
+        // const jsonObj = JSON.parse("hello");
+        const jsonObj = {'name':'David'}
+        res.json(jsonObj);
+    } catch (err) {
+        console.error("Error:", err);
+        res.status(500).send("Hmmm, something smells... No socks for you! ☹");
+    }
+});
+
+app.get('/api/planets/:id/films', async (req, res) => {
+    try {
+        // Console log the entire request object
+        console.log(req);
+
+        // const data = await fs.readFile('../data/socks.json', 'utf8');
+        // const jsonObj = JSON.parse("hello");
+        const jsonObj = {'name':'David'}
+        res.json(jsonObj);
+    } catch (err) {
+        console.error("Error:", err);
+        res.status(500).send("Hmmm, something smells... No socks for you! ☹");
+    }
+});
+
+app.get('/api/planets/:id/characters', async (req, res) => {
     try {
         // Console log the entire request object
         console.log(req);
