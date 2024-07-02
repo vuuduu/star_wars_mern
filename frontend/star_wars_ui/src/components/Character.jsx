13be471ea338b23c5a8c3b43da23c4edf0b60111
Abhay Bisht <abhay@webage.com>
2024-07-02 14:08:27 -0400

    Finished character component and added new route to server

--- frontend/star_wars_ui/src/components/Character.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-
-const Character = () => {
-    
-
-};
-
-export default Character;
--- frontend/star_wars_ui/src/components/Form/Character.jsx
+++ frontend/star_wars_ui/src/components/Form/Character.jsx
@@ -1,11 +1,48 @@
 import { useParams } from "react-router-dom";
 import { Link } from "react-router-dom";
+import {useState, useEffect} from 'react';
 
 const Character = () => {
     const { characterid } = useParams();
+    const [character, setCharacter] = useState();
+    const [homeworld, setHomeworld] = useState();
+    const [films, setFilms] = useState();
+
+    useEffect(() => {
+        async function fetchData() {
+            // TODO: Abstract base URI to .env
+            // Can optimize using Promise.all
+            const character = await fetch(`http://localhost:3000/api/characters/${characterid}`).then(res => res.json());
+            const homeworld = await fetch(`http://localhost:3000/api/characters/${characterid}/planet`).then(res => res.json());
+            const films = await fetch(`http://localhost:3000/api/characters/${characterid}/films`).then(res => res.json());
+            setCharacter(character);
+            setHomeworld(homeworld);
+            setFilms(films);
+        }
+        fetchData();
+    }, []);
 
     return (
-        <h1>Characters id: {characterid}</h1>
+        <main>
+            <h1 id="name">{character?.name}</h1>
+            <section id="generalInfo">
+                <p>Height: <span id="height">{character?.height}</span> cm</p>
+                <p>Mass: <span id="mass">{character?.mass}</span> kg</p>
+                <p>Born: <span id="birth_year">{character?.birth_year}</span></p>
+            </section>
+            <section id="planets">
+                <h2>Homeworld</h2>
+                <p><span id="homeworld"><a onClick={() => window.location = `/planet/${homeworld?.id}`}>{homeworld?.name}</a></span></p>
+            </section>
+            <section id="films">
+                <h2>Films appeared in</h2>
+                <ul>
+                    {films?.map((film) =>
+                        <li><a onClick={() => window.location = `/film/${film?.id}`}>{film?.title}</a></li>
+                    )}
+                </ul>
+            </section>
+        </main>
     )
 }
 
